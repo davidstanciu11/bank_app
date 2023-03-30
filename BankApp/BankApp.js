09b2039690fc6ab92b7e3692d1778ef035a12196
davidstanciu11 <stanciudavid169@gmail.com>
2023-03-30 17:36:09 +0300

    operations tab

--- BankApp/BankApp.js
+++ BankApp/BankApp.js
@@ -1,13 +1,19 @@
-
 const modal = document.getElementById("modal");
 const overlay = document.getElementById("overlay");
 const btnCloseModal = document.getElementById("btn_modal");
 const btnOpenModal = document.getElementById("btn_account");
+const btnOperationTransfer = document.getElementById("btn_operation1");
+const btnOperationLoan = document.getElementById("btn_operation2");
+const btnOperationDelete = document.getElementById("btn_operation3");
+const operationTransferContainer = document.getElementById("data_1");
+const operationLoanContainer = document.getElementById("data_2");
+const operationDeleteContainer = document.getElementById("data_3");
 
+let buttons = true;
 const openModal = () => {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-}
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
+};
 
 const closeModal = () => {
 	modal.classList.add("hidden");
@@ -17,5 +23,29 @@ const closeModal = () => {
 btnOpenModal.addEventListener("click", openModal);
 btnCloseModal.addEventListener("click", closeModal);
 document.addEventListener("keydown", (e) => {
-    e.key === "Escape" && !modal.classList.contains("hidden") ? closeModal() : modal;
-})
+	e.key === "Escape" && !modal.classList.contains("hidden")
+		? closeModal()
+		: modal;
+});
+
+btnOperationTransfer.addEventListener("click", () => {
+	btnOperationTransfer.style.transform = buttons ? "translateY(-5px)" : "translateY(0)";
+	btnOperationLoan.style.transform = "translateY(0)";
+	btnOperationDelete.style.transform = "translateY(0)";
+	buttons = !buttons;
+
+});
+
+btnOperationLoan.addEventListener("click", () => {
+	btnOperationTransfer.style.transform = "translateY(0)";
+	btnOperationLoan.style.transform = buttons ? "translateY(-5px)" : "translateY(0)";
+	btnOperationDelete.style.transform = "translateY(0)";
+	buttons = !buttons;
+});
+
+btnOperationDelete.addEventListener("click", () => {
+	btnOperationTransfer.style.transform = "translateY(0)";
+	btnOperationLoan.style.transform = "translateY(0)";
+	btnOperationDelete.style.transform = buttons ? "translateY(-5px)" : "translateY(0)";
+	buttons = !buttons;
+});
