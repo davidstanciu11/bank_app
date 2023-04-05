72faa2182bb4602175b4221d7205583cf796d400
davidstanciu11 <stanciudavid169@gmail.com>
2023-04-05 16:56:29 +0300

    restrutured code

--- BankApp/BankApp.js
+++ BankApp/BankApp.js
@@ -10,9 +10,31 @@ const operationLoanContainer = document.getElementById("data_2");
 const operationDeleteContainer = document.getElementById("data_3");
 const btnScroll = document.getElementById("btn_scroll");
 const navigation = document.querySelector(".nav_list");
+const hiddenSections = document.querySelectorAll(".section_hidden");
+const slides = document.querySelectorAll(".slide");
+const btnLeftSlider = document.querySelector(".slider_btn_left");
+const btnRightSlider = document.querySelector(".slider_btn_right");
 
+//Operation Container
 operationLoanContainer.classList.remove("card_operation_active");
 operationDeleteContainer.classList.remove("card_operation_active");
+const setTheOperation = (
+	activeBtn,
+	inactiveButton,
+	inactiveButton2,
+	activeContainer,
+	inactiveContainer,
+	inactiveContainer2
+) => {
+	activeBtn.style.transform = "translateY(-5px)";
+	inactiveButton.style.transform = "translateY(0)";
+	inactiveButton2.style.transform = "translateY(0)";
+	inactiveContainer.classList.remove("card_operation_active");
+	inactiveContainer2.classList.remove("card_operation_active");
+	activeContainer.classList.add("card_operation_active");
+};
+
+//ScrollToTop button
 
 window.onscroll = () => {
 	scrollToTop();
@@ -29,6 +51,8 @@ const getToTop = () => {
 	document.documentElement.scrollTop = 0;
 };
 
+//Modal
+
 const openModal = () => {
 	modal.classList.remove("hidden");
 	overlay.classList.remove("hidden");
@@ -38,6 +62,8 @@ const closeModal = () => {
 	modal.classList.add("hidden");
 	overlay.classList.add("hidden");
 };
+
+//Nav Blur Effect
 
 const blurNav = (e, opacity) => {
 	if (e.target.classList.contains("nav__link")) {
@@ -54,6 +80,31 @@ const blurNav = (e, opacity) => {
 	}
 };
 
+//Animation onScroll
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add("section_show");
+		} else {
+			entry.target.classList.remove("section_show");
+		}
+	});
+});
+hiddenSections.forEach((el) => observer.observe(el));
+
+//Slider
+
+let curSlide = 0;
+const maxSlide = slides.length;
+const goToSlide = (slide) => {
+	slides.forEach(
+		(s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+	);
+};
+goToSlide(0);
+//EventListeners
+
 btnOpenModal.addEventListener("click", openModal);
 btnCloseModal.addEventListener("click", closeModal);
 document.addEventListener("keydown", (e) => {
@@ -63,31 +114,36 @@ document.addEventListener("keydown", (e) => {
 });
 
 btnOperationTransfer.addEventListener("click", () => {
-	btnOperationTransfer.style.transform = "translateY(-5px)";
-	btnOperationLoan.style.transform = "translateY(0)";
-	btnOperationDelete.style.transform = "translateY(0)";
-
-	operationLoanContainer.classList.remove("card_operation_active");
-	operationDeleteContainer.classList.remove("card_operation_active");
-	operationTransferContainer.classList.add("card_operation_active");
+	setTheOperation(
+		btnOperationTransfer,
+		btnOperationLoan,
+		btnOperationDelete,
+		operationTransferContainer,
+		operationLoanContainer,
+		operationDeleteContainer
+	);
 });
 
 btnOperationLoan.addEventListener("click", () => {
-	btnOperationTransfer.style.transform = "translateY(0)";
-	btnOperationLoan.style.transform = "translateY(-5px)";
-	btnOperationDelete.style.transform = "translateY(0)";
-	operationTransferContainer.classList.remove("card_operation_active");
-	operationDeleteContainer.classList.remove("card_operation_active");
-	operationLoanContainer.classList.add("card_operation_active");
+	setTheOperation(
+		btnOperationLoan,
+		btnOperationTransfer,
+		btnOperationDelete,
+		operationLoanContainer,
+		operationTransferContainer,
+		operationDeleteContainer
+	);
 });
 
 btnOperationDelete.addEventListener("click", () => {
-	btnOperationTransfer.style.transform = "translateY(0)";
-	btnOperationLoan.style.transform = "translateY(0)";
-	btnOperationDelete.style.transform = "translateY(-5px)";
-	operationTransferContainer.classList.remove("card_operation_active");
-	operationDeleteContainer.classList.add("card_operation_active");
-	operationLoanContainer.classList.remove("card_operation_active");
+	setTheOperation(
+		btnOperationDelete,
+		btnOperationTransfer,
+		btnOperationLoan,
+		operationDeleteContainer,
+		operationTransferContainer,
+		operationLoanContainer
+	);
 });
 
 btnScroll.addEventListener("click", getToTop);
@@ -98,4 +154,9 @@ navigation.addEventListener("mouseover", (e) => {
 
 navigation.addEventListener("mouseout", (e) => {
 	blurNav(e, 1);
+});
+
+btnRightSlider.addEventListener("click", () => {
+	curSlide === maxSlide - 1 ? (curSlide = 0) : curSlide++;
+	goToSlide(curSlide);
 });
